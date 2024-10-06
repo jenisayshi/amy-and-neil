887ec3c9b6f29a4ceab397596eb7aa4114133df9
jenisayshi <166698885+jenisayshi@users.noreply.github.com>
2024-10-06 19:58:27 +0100

    pov: you discovered bootstrap exists

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -14,19 +14,17 @@ var x = setInterval(function() {
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("countdown").innerHTML = days + " days " + hours + " hours "
-  + minutes + " minutes " + seconds + " seconds ";
+  document.getElementById("countdown-text").innerHTML = days + " days " + hours + " hours "
+  + minutes + " minutes ";
 
-  document.getElementById("countdown-placeholder").style.display = "none";
-  document.getElementById("countdown").style.display = "block";  
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = "jeni says hi";
+    document.getElementById("countdown-text").innerHTML = "Congratulations to Mrs and Mr Farnan!!!";
   }
 }, 1000);
 
