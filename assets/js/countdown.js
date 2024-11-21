12522311b9c65576b449ce7312b923c3acb2ddb9
jenisayshi <166698885+jenisayshi@users.noreply.github.com>
2024-11-21 14:19:20 +0000

    remove table from q-and-a

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Apr 25, 2025 12:00:00").getTime();
+var countDownDate = new Date("Apr 25, 2025 14:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -12,19 +12,16 @@ var x = setInterval(function() {
 
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  // var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("countdown-text").innerHTML = days + " days " + hours + " hours "
-  + minutes + " minutes ";
+  document.getElementById("countdown-text").innerHTML = days + " days "
 
+  document.getElementById("countdown-placeholder").style.display = "none";
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown-text").innerHTML = "Congratulations to Mrs and Mr Farnan!!!";
+    document.getElementById("countdown-text").innerHTML = "Congratulations to Amy and Neil!";
   }
 }, 1000);
 
