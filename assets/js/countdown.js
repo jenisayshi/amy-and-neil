153105a7a552dc72f956486eb86a2d089612f7dc
jenisayshi <166698885+jenisayshi@users.noreply.github.com>
2024-12-15 18:03:49 +0000

    fix countdown

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -12,9 +12,11 @@ var x = setInterval(function() {
 
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 
   // Display the result in the element with id="demo"
-  document.getElementById("countdown-text").innerHTML = days + " days "
+  document.getElementById("countdown-text").innerHTML = days + " days<br> "  + hours + " hours "
+  
 
   document.getElementById("countdown-placeholder").style.display = "none";
 
